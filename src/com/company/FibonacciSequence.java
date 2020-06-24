2d27cbfb4e64d2e6dbd28964bd3b6375b01d2495
brandon <brandon.fitzpatrick8154@gmail.com>
2020-06-24 18:42:42 -0500

    built method that prints the fibonacci sequence

--- src/com/company/FibonacciSequence.java
+++ src/com/company/FibonacciSequence.java
@@ -1,8 +1,33 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class FibonacciSequence {
 
     public static void fibonacciSequenceCalculator(){
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("This program writes the Fibonacci Sequence to any given amount of element");
+        System.out.println("Please type how many elements of the Fibonacci Sequence you would like printed:");
+        int elements = sc.nextInt();
+        System.out.println("Fibonacci Sequence:\n");
+        int oneAway = 0;
+        int twoAway = 0;
+        int currentNum;
+        for (int elementIter = 0; elementIter < elements; elementIter++){
+
+            if (elementIter == 0){
+                currentNum = 0;
+                twoAway = currentNum;
+            } else if (elementIter == 1){
+                currentNum = 1;
+                oneAway = currentNum;
+            } else {
+                currentNum = oneAway + twoAway;
+                twoAway = oneAway;
+                oneAway = currentNum;
+            }
+
+            System.out.println(currentNum + "\n");
+        }
     }
 }
