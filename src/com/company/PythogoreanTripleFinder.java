64ba26df9ef3ed7d30f682923664c294960e8f53
brandon <brandon.fitzpatrick8154@gmail.com>
2020-08-11 08:20:43 -0500

    adding return statement

--- src/com/company/PythogoreanTripleFinder.java
+++ src/com/company/PythogoreanTripleFinder.java
@@ -5,7 +5,10 @@ public class PythogoreanTripleFinder {
     //Given an array of integers, determine whether it contains a Pythagorean triplet.
 
     public static int[] PythogoreanTripleFinder(int[] numArr){
+        int[] pTriple = new int[3];
 
+
+        return pTriple;
     }
 
 }
