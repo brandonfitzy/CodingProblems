03527c93b371afcfec984631fff3c330e71ab888
brandon <brandon.fitzpatrick8154@gmail.com>
2020-08-11 08:19:29 -0500

    adding method

--- src/com/company/PythogoreanTripleFinder.java
+++ src/com/company/PythogoreanTripleFinder.java
@@ -4,4 +4,8 @@ public class PythogoreanTripleFinder {
 
     //Given an array of integers, determine whether it contains a Pythagorean triplet.
 
+    public static int[] PythogoreanTripleFinder(int[] numArr){
+
+    }
+
 }
