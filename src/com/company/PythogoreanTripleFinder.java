4c5e68cb46cea4b7cb7f4fd13d348d9a1fbbf0d3
brandon <brandon.fitzpatrick8154@gmail.com>
2020-08-12 10:21:09 -0500

    edits

--- src/com/company/PythogoreanTripleFinder.java
+++ src/com/company/PythogoreanTripleFinder.java
@@ -1,11 +1,19 @@
 package com.company;
 
-public class PythogoreanTripleFinder {
+public class PythagoreanTripleFinder {
 
     //Given an array of integers, determine whether it contains a Pythagorean triplet.
 
-    public static int[] PythogoreanTripleFinder(int[] numArr){
+    public static int[] PythogareanTripleFinder(int[] numArr){
+        int a; int b; int c;
         int[] pTriple = new int[3];
+
+        if ((a^2)+(b^2) == (c^2)){
+            pTriple[0] = a;
+            pTriple[1] = b;
+            pTriple[2] = c;
+            System.out.println("There is a Pythagorean ");
+        } else
 
 
         return pTriple;
